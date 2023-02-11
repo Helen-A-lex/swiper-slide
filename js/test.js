e9c55ce8bb3f37d4af5b6023f96cc3ef9cdbdec4
Helen-A-lex <elenahomutovskaa652@gmail.com>
2023-02-11 23:03:18 +0100

    add new function js

--- js/test.js
+++ js/test.js
@@ -77,3 +77,200 @@
 //   return canAccessContent;
 // }
 // console.log(checkIfCanAccessContent("pro"));
+
+23;
+// function isNumberNotInRange(start, end, number) {
+//   const isInRange = number >= start && number <= end;
+//   const isNotInRange = !isInRange; // Change this line
+
+//   return isNotInRange;
+// }
+// console.log(isNumberNotInRange(10, 30, 5));
+
+24; //  задача про дисконти
+// function getDiscount(totalSpent) {
+//   const BASE_DISCOUNT = 0;
+//   const BRONZE_DISCOUNT = 0.02;
+//   const SILVER_DISCOUNT = 0.05;
+//   const GOLD_DISCOUNT = 0.1;
+//   let discount;
+//   // Change code below this line
+//   if (totalSpent < 5000) {
+//     console.log(`знижка 0 базовий партнер`);
+//     discount = BASE_DISCOUNT;
+//   } else if (totalSpent >= 5000 && totalSpent < 20000) {
+//     console.log(`знижка 2% бронзовий партнер`);
+//     discount = BRONZE_DISCOUNT;
+//   } else if (totalSpent >= 20000 && totalSpent < 50000) {
+//     console.log(`знижка 5% срібний партнер`);
+//     discount = SILVER_DISCOUNT;
+//   } else {
+//     console.log(`знижка 10% золотий партнер`);
+//     discount = GOLD_DISCOUNT;
+//   }
+//   // Change code above this line
+//   return discount;
+// }
+// console.log(getDiscount(20000));
+
+25; //  тернарний оператор
+// function checkStorage(available, ordered) {
+//   let message;
+//   // Change code below this line
+//   message =
+//     ordered > available
+//       ? "Not enough goods in stock!"
+//       : "The order is accepted, our manager will contact you";
+//   if (ordered > available) {
+//     message = "Not enough goods in stock!";
+//   } else {
+//     message = "The order is accepted, our manager will contact you";
+//   }
+
+//   // Change code above this line
+//   return message;
+// }
+
+26; // function checkPassword(password) {
+//   const ADMIN_PASSWORD = "jqueryismyjam";
+//   let message;
+//   // Change code below this line
+//   message =
+//     password === ADMIN_PASSWORD
+//       ? "Access is allowed"
+//       : "Access denied, wrong password!";
+//   // Change code above this line
+//   return message;
+// }
+// console.log(checkPassword("BNBJJNKJKHKJ"));
+
+27; //  СВІТЧ// function getSubscriptionPrice(type) {
+//   let price;
+//   // Change code below this line
+
+//   switch (
+//     type // Change this line
+//   ) {
+//     case `starter`: // Change this line
+//       price = 0; // Change this line
+//       break;
+
+//     case `professional`: // Change this line
+//       price = 20; // Change this line
+//       break;
+
+//     case `organization`: // Change this line
+//       price = 50; // Change this line
+//       break;
+//   }
+
+//   // Change code above this line
+//   return price;
+// }
+// console.log(getSubscriptionPrice("professional"));
+
+28; // світч з дефолтом// function checkPassword(password) {
+//   const ADMIN_PASSWORD = "jqueryismyjam";
+//   let message;
+//   // Change code below this line
+//   switch (password) {
+//     case `null`:
+//       message = "Canceled by user!";
+//       break;
+
+//     case `ADMIN_PASSWORD`:
+//       message = "Welcome!";
+//       break;
+
+//     default:
+//       message = "Access denied, wrong password!";
+//   }
+//   if (password === null) {
+//     message = "Canceled by user!";
+//   } else if (password === ADMIN_PASSWORD) {
+//     message = "Welcome!";
+//   } else {
+//     message = "Access denied, wrong password!";
+//   }
+
+//   // Change code above this line
+//   return message;
+// }
+
+29; //  світч із цінами// function getShippingCost(country) {
+//   let message;
+//   // Change code below this line
+//   switch (country) {
+//     case `China`:
+//       message = `Shipping to ${country} will cost ${100} credits`;
+//       break;
+
+//     case `Jamaica`:
+//       message = `Shipping to ${country} will cost ${120} credits`;
+//       break;
+
+//     case `Australia`:
+//       message = `Shipping to ${country} will cost ${170} credits`;
+//       break;
+
+//     case `Chile`:
+//       message = `Shipping to ${country} will cost ${250} credits`;
+//       break;
+
+//     default:
+//       message = `Sorry, there is no delivery to your country`;
+//   }
+//   // Change code above this line
+//   return message;
+// }
+// console.log(getShippingCost("Germany"));
+
+30; //  довжина рядка// function getNameLength(name) {
+//   const message = `Name ${name} is ${name.length} characters long`; // Change this line
+
+//   return message;
+// }
+// console.log(getNameLength(`illinois`));
+
+31; //  визначення довжини рядка (індекс останнього елемента рядка на 1 менший за його довжину// const courseTopic = "JavaScript essentials";
+// // Change code below this line
+
+// const courseTopicLength = courseTopic.length;
+// const firstElement = courseTopic[0];
+// const lastElement = courseTopic[courseTopic.length - 1];
+
+// // Change code above this line
+
+32; // вираз створення підрядка довжиною length символів (від початку) з рядка string
+// function getSubstring(string, length) {
+//   const substring = string.slice(0, length); // Change this line
+
+//   return substring;
+// }
+// console.log(getSubstring("Hello world", 3));
+
+33; //  обрізається рядок макслендс і додається три крапки
+// function formatMessage(message, maxLength) {
+//   let result;
+//   // Change code below this line
+//   if (message.length <= maxLength) {
+//     result = message;
+//   } else {
+//     result = message.slice(0, maxLength) + "...";
+//   }
+//   /// Change code above this line
+//   return result;
+// }
+
+34; //  приведення тексту месенджа до нижнього регістра, а потім перевірка на наявність слів спам і сейл
+// function checkForSpam(message) {
+//   let result;
+//   // Change code below this line
+//   const normalizedMessage = message.toLowerCase();
+//   result =
+//     normalizedMessage.includes("spam") || normalizedMessage.includes("sale");
+
+//   // Change code above this line
+//   return result;
+// }
+// console.log(checkForSpam("Get best sale offers now!"));
