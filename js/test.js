5f382b7122d9bd33c3b318bbdea727daef85bfbc
Helen-A-lex <elenahomutovskaa652@gmail.com>
2023-02-22 22:29:45 +0100

    add new commit

--- js/test.js
+++ js/test.js
@@ -582,24 +582,65 @@
 //   includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Jupiter")
 // );
 
-// 3 модуль
-const apartment = {
-  imgUrl: "https://via.placeholder.com/640x480",
-  descr: "Spacious apartment in the city center",
-  rating: 4.7,
-  price: 5000,
-  tags: ["premium", "promoted", "top", "trusted"],
-  owner: {
-    name: "Henry Sibola",
-    phone: "982-126-1588",
-    email: "henry.carter@aptmail.com",
-  },
-};
+// 3 модуль Об'єкти
 
-// Change code below this line
-apartment.area = 60;
-apartment.rooms = 3;
-apartment.location = {};
-apartment.location.country = "Jamaica";
-apartment.location.city = "Kingston";
-console.log(apartment);
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4.7,
+//   price: 5000,
+//   tags: ["premium", "promoted", "top", "trusted"],
+//   owner: {
+//     name: "Henry Sibola",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// // Change code below this line
+// apartment.area = 60;
+// apartment.rooms = 3;
+// apartment.location = {};
+// apartment.location.country = "Jamaica";
+// apartment.location.city = "Kingston";
+// console.log(apartment);
+
+// Синтаксис коротких властивостей
+8; // const name = "Repair Droid";
+// const price = 2500;
+// const image = "https://via.placeholder.com/640x480";
+// const tags = ["on sale", "trending", "best buy"];
+
+// const product = {
+//   name,
+//   price,
+//   image,
+//   tags,
+// };
+// console.log(product);
+
+// Синтаксис обчислювальних властивостей
+9; // const emailInputName = "email";
+// const passwordInputName = "password";
+
+// const credentials = {
+//   [emailInputName]: "henry.carter@aptmail.com",
+//   [passwordInputName]: "jqueryismyjam",
+// };
+// console.log(credentials);
+
+10;//  Повернення масиву ключів та масиву властивостей
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const keys = [];
+// const values = [];
+// // Change code below this line
+// for (const key in apartment) {
+//   keys.push(key);
+//   values.push(apartment[key]);
+// }
+// console.log(keys);
+// console.log(values);
