8fec199cfb2b569da5020fac75e1f89ce0ced5f2
Helen-A-lex <elenahomutovskaa652@gmail.com>
2023-03-10 20:27:53 +0100

    add task

--- js/test.js
+++ js/test.js
@@ -2043,3 +2043,28 @@
 // console.log(mango.blacklistedEmails); // ["poly@mail.com"]
 // console.log(mango.isBlacklisted("mango@mail.com")); // false
 // console.log(mango.isBlacklisted("poly@mail.com")); // true
+
+// Самостійне опрацювання
+// Напишіть клас Toggle який приймає об'єкт налаштувань {isOpen: boolean} і оголошує одну властивість on - стан вкл/викл (true/false). За замовчуванням значення властивості on повинно бути false.
+// class Toggle {
+//   constructor({ isOpen = false }) {
+//     this.on = isOpen;
+//   }
+//   toggle() {
+//     this.on = !this.on;
+//   }
+// }
+
+// const firstToggle = new Toggle({ isOpen: true });
+// console.group("firstToggle");
+// console.log(firstToggle.on);
+// firstToggle.toggle();
+// console.log(firstToggle.on);
+// console.groupEnd("firstToggle");
+
+// const secondToggle = new Toggle();
+// console.group("secondToggle");
+// console.log(secondToggle.on);
+// secondToggle.toggle();
+// console.log(secondToggle.on);
+// console.groupEnd("secondToggle");
